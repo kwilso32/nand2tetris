d4183947c562623d32ca324b5ea67e1c66faf19c
kenreidwilson <kenreidwilson@gmail.com>
2019-09-11 15:47:45 -0400

    Added Base Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,27 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@8191
+D=A
+@count
+M=D
+(LOOP_START)
+@count
+D=M
+M=M-1
+@16384
+A=D+A
+M=-1
+@count
+D=M
+@LOOP_START
+D;JGT
+(PGM_END)
+@PGM_END
+0;JMP
